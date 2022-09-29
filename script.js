3d772fa30104663b635bb96387ee3befdf48ca6a
vadim-sobinin <vadimsobinin@yandex.ru>
2022-09-29 22:40:39 +0300

    lesson01

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = "Monitors";
+const screens = '27\" and 32\"';
+const screenPrice = "200$ and 300$";
+const rollback = "15%";
+const fullPrice = 250;
+const adaptive = true;
+
+alert("Alert is working!");
+console.log("Console output is working!");
+

79c9600c003b0c96a32fa66d9ef7f9b66d4b12ea
vadim-sobinin <vadimsobinin@yandex.ru>
2022-09-30 10:30:26 +0300

    My homework for lesson02

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Sites";
+let screens = "Simple, Complex, Interactive";
+let screenPrice = 100;
+let rollback = 20;
+let fullPrice = 120;
+let adaptive = true;
 
-alert("Alert is working!");
-console.log("Console output is working!");
-
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Layouts price is " + screenPrice + " USD.");
+console.log("Site development price is " + fullPrice + " USD.");
+console.log(screens.toLowerCase().split(", "));
+console.log("Percentage of payback to the broker for the work: " + (fullPrice * (rollback / 100)));

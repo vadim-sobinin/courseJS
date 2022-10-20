47bdd82ef28eff3c9c86476090700b57ddd24814
vadim-sobinin <vadimsobinin@yandex.ru>
2022-10-20 19:35:52 +0300

    complete lesson12

--- js/script.js
+++ js/script.js
@@ -13,12 +13,12 @@ const rollbackRange = document.querySelector(".rollback > .main-controls__range 
 const rollbackRangeNumber = document.querySelector(".rollback > .main-controls__range > span");
 
 const total = Array.from(document.getElementsByClassName("total-input"))[0];
-const totalCount = Array.from(document.getElementsByClassName("total-input"))[1];
+const totalNumber = Array.from(document.getElementsByClassName("total-input"))[1];
 const totalCountOther = Array.from(document.getElementsByClassName("total-input"))[2];
 const fullTotalCount = Array.from(document.getElementsByClassName("total-input"))[3];
 const totalCountRollback = Array.from(document.getElementsByClassName("total-input"))[4];
 
-let screens = document.querySelectorAll(".screen");
+let screens;
 
 
 
@@ -28,8 +28,9 @@ let screens = document.querySelectorAll(".screen");
 const appData = {
   title: "",
   screens: new Array(),
+  count: 0,
   screenPrice: 0,
-  rollback: 20,
+  rollback: 0,
   adaptive: true,
   servicesPercent: {},
   servicesNumber: {},
@@ -41,8 +42,10 @@ const appData = {
   init: function(){
     
     appData.addTitle();
-
+    // appData.checkBlankInputs();
     calculateBtn.addEventListener('click', appData.start);
+
+    rollbackRange.addEventListener('input', appData.rollbackRangeChange);
 
     plusBtn.addEventListener('click', appData.addScreenBlock);
   },
@@ -52,7 +55,7 @@ const appData = {
   },
 
   start: function () {
-    
+    console.log("Start");
     appData.addScreens();
     appData.addServices();
 
@@ -70,16 +73,25 @@ const appData = {
     total.value = appData.screenPrice.price;
     totalCountOther.value = appData.servicePricesPercent + appData.servicePricesNumber;
     fullTotalCount.value = appData.fullPrice;
+    totalCountRollback.value = appData.servicePercentPrice;
+    totalNumber.value = appData.count;
     
   },
 
   addScreens: function(){
+    appData.count = 0;
+
+    appData.screens = [];
+
     screens = document.querySelectorAll(".screen");
 
     screens.forEach(function(screen, index){
       const select = screen.querySelector('select');
       const input = screen.querySelector('input');
       const selectName = select.options[select.selectedIndex].textContent.split(" ")[0];
+
+      appData.count++;
+
       appData.screens.push({
         id: index,
         name: selectName,
@@ -87,6 +99,31 @@ const appData = {
       });
       
     });
+
+  },
+
+  checkBlankInputs: function(){
+    calculateBtn.disabled = "false";
+    screens.forEach((screen) => {
+      const select = screen.querySelector("select");
+      const input = screen.querySelector("input");
+      if (select.value == "") {
+        calculateBtn.disabled = "true";
+        console.log("Check select");
+      }
+      if (isNaN(input.value) || input.value == 0){
+        calculateBtn.disabled = "true";
+        console.log("Check input");
+      }
+    });
+    
+  },
+
+  rollbackRangeChange: function(){
+    rollbackRangeNumber.textContent = `${rollbackRange.value}%`;
+    appData.rollback = +rollbackRange.value;
+    appData.servicePercentPrice = Math.round(appData.fullPrice * ((100 - appData.rollback) / 100));
+    totalCountRollback.value = appData.servicePercentPrice;
 
   },
 
@@ -117,7 +154,8 @@ const appData = {
   },
 
   addPrices: function(){
-    appData.screenPrice = appData.screens.reduce(((total, item) => total + item));
+    appData.screenPrice = 0;
+    appData.screenPrice = appData.screens.reduce(((total, item) =>  total + item.price), 0);
 
     for (let key in appData.servicesNumber){
       appData.servicePricesNumber += appData.servicesNumber[key];
@@ -129,29 +167,14 @@ const appData = {
       appData.servicePricesPercent += appData.screenPrice.price * (appData.servicesPercent[key] / 100);
     }
 
-    appData.fullPrice = +appData.screenPrice.price + appData.servicePricesPercent + appData.servicePricesNumber;
+    appData.fullPrice = +appData.screenPrice + appData.servicePricesPercent + appData.servicePricesNumber;
+
+    appData.servicePercentPrice = Math.round(appData.fullPrice * ((100 - appData.rollback) / 100));
+
   },
 
   isNumber: function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
-  },
-
-
-
-  getServicePercentPrices: function (fullPrice, servicePercentPrice) {
-    appData.servicePercentPrice =  fullPrice - Math.round(fullPrice * (appData.rollback / 100));
-  },
-
-  getRollbackMessage: function (fullPrice) {
-    if (fullPrice >= 500) {
-      return "Your discount is 10%";
-    } else if (fullPrice >= 250) {
-      return "Your discount is 5%";
-    } else if (fullPrice >= 0 && fullPrice < 250) {
-      return "You have no discount.";
-    } else {
-      return "Oops... Something went wrong.";
-    }
   },
 
   showTypeOf: function (variable) {
